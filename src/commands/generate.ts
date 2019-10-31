05efb1d4981ce1fc66ceb18535b8ae5fd8c311f9
Bobby Johnson <Bobby@MadnessLabs.net>
2019-10-31 04:29:24 -0500

    fix(): Remove dist and tweak generate command

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -2,6 +2,8 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 export default async () => {
-  const { stdout, stderr } = await exec(`npm run g:${process.argv[3]}`);
+  const { stdout, stderr } = await exec(
+    `exec < /dev/tty; npm run g:${process.argv[3]}`
+  );
   console.log(stdout, stderr);
 };
