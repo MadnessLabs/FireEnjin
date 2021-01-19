56026ec3853157a0f0ef78c81e2ae11d98e3e429
Bobby Johnson <bobby@madnesslabs.net>
2021-01-19 00:35:51 -0600

    fix(presets): Add custom define method to handle binding presets to window

--- src/commands/presets.ts
+++ src/commands/presets.ts
@@ -1,15 +1,25 @@
 import * as fg from "fast-glob";
 import { exec } from 'child_process';
+import prependFile from "prepend-file";
 
 export default async () => {
   const entries = fg.sync(['src/**/*.presets.ts']); 
   if (!entries?.length) throw new Error("No presets found...");
   const command = `tsc ${entries.join(" ")} --out ${process.argv[3] && process.argv[3] !== "watch" ? process.argv[3] : "www/presets.js"} --module amd ${process.argv[3] === 'watch' || process.argv[4] === 'watch' ? '--watch' : ""} --moduleResolution node`;
 
-  exec(command, function(error, stdout, stderr) {
+  exec(command, async function(error, stdout, stderr) {
       if (error !== null) {
           console.log('exec error: ' + error, stdout, stderr);
       }
+      await prependFile(process.argv[3] && process.argv[3] !== "watch" ? process.argv[3] : "www/presets.js", `
+function define(name, dependencies, callback) {
+  let exports = {};
+  callback({}, exports);
+  if (!window.presets) window.presets = {};
+  window.presets[name] = exports.default;
+};
+
+      `);
       console.log('Completed rendering presets...')
   });
 };
